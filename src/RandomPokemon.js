f0a9532ed892f07b2ebf953b3dc2b877b4b90ca5
Shakila101 <167652608+101Shakila@users.noreply.github.com>
2024-09-05 13:43:49 -0400

    Random Pokemon function

---
+++ src/RandomPokemon.js
@@ -0,0 +1,14 @@
+import "./RandomPokemon.css"
+
+export default function RandomPokemon() {
+
+    return (
+        <div className="PokeBoarder">
+            <h1>Pokemon #{ }</h1>
+            <image src="" />
+        </div>
+    )
+
+};
+
+
