8f23b2231893d977089e148f62767e2a37e8105a
Shakila101 <167652608+101Shakila@users.noreply.github.com>
2024-09-05 00:28:34 -0400

    Component Decomposition

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Shakila from './Shakila';
 import Dog from './Dog';
-import Die from './Die';
+import DieRoll from './DieRoll';
 
 //testing out the functionalities of react.
 function App() {
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <Shakila />
       <Dog />
-
+      <DieRoll />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
