46fab0dc61b64777f072358c230d3bd544c0a2bc
Shakila101 <167652608+101Shakila@users.noreply.github.com>
2024-09-05 00:27:57 -0400

    DieRoll creation

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <Shakila />
       <Dog />
-      <Die />
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/DieRoll.js
@@ -0,0 +1,12 @@
+import Die from './Die';
+
+
+export default function DieRoll() {
+    return (
+        <div>
+            <Die />
+            <Die />
+            <Die />
+        </div>
+    );
+}
