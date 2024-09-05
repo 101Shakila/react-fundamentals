2779d3b98218ea2947553be47eed70445457bfd6
Shakila101 <167652608+101Shakila@users.noreply.github.com>
2024-09-04 23:58:38 -0400

    using JS expression inside component

--- src/Dog.js
+++ src/Dog.js
@@ -1,9 +1,11 @@
 function Dog() {
     //JSX RULES - You must explicity close self-closing elements like <br/>
     //ANOTHER RULES IS  - Components can only return a single element.
+    const pet = "lily";
+
     return (
         <div className='App'>
-            <h2>What the dog doing here?</h2>
+            <h2>What the dog {pet} doing here?</h2>
         </div>
     )
 }
